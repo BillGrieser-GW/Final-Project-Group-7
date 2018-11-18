a52108bde00742820b33d7759d7b1efad75ba582
bill_grieser <bill.grieser@rollingstorm.com>
2018-11-18 10:52:23 -0500

    About to reorganize

--- code/bill_net1.py
+++ code/bill_net1.py
@@ -9,13 +9,14 @@
 import os
 import sys
 import torch
-import torchvision
+
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn as nn
 from torch.autograd import Variable
 import datetime
+import time
 
 from svhnpickletypes import SvhnDigit
 from svhndatasets import SvhnDigitsDataset
@@ -29,14 +30,14 @@ import pickle
 # and a moderate number of neurons and learning rate
 # 
 
-IMAGE_SIZE = (32,32)
+IMAGE_SIZE = (52,52)
 CHANNELS = 1
-input_size = (CHANNELS * IMAGE_SIZE[0] * IMAGE_SIZE[1]) 
+INPUT_SIZE = (CHANNELS * IMAGE_SIZE[0] * IMAGE_SIZE[1]) 
 hidden_size = 500
 num_classes = 10
-num_epochs = 2
-batch_size = 250
-learning_rate = .01
+num_epochs = 20
+batch_size = 128
+learning_rate = .005
 
 FORCE_CPU = False
 
@@ -56,7 +57,7 @@ else:
 transform = transforms.Compose([transforms.Grayscale(),
                                 transforms.Resize(IMAGE_SIZE),
                                 transforms.ToTensor(), 
-                                transforms.Normalize((0.5,), (0.5,))])
+                                transforms.Normalize((0.5,) * CHANNELS, (0.5,) * CHANNELS)])
 # --------------------------------------------------------------------------------------------
 
 DATA_DIR = os.path.join("..", "data")
@@ -79,54 +80,59 @@ test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuff
 # =============================================================================
 # Set up the network
 # =============================================================================
-# --------------------------------------------------------------------------------------------
-# Choose the right argument for xx
 #
 # Define a model class. This uses purelin() as the second-layer
 # transfer function
 class FlatNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(FlatNet, self).__init__()
+        self.input_size = input_size
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
-        out = self.fc1(x)
+        out = x.view(-1, self.input_size)  # Flatten
+        out = self.fc1(out)
         out = self.relu(out)
         out = self.fc2(out)
         return out
     
 class ConvNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
+    def __init__(self, hidden_size, num_classes):
         super(ConvNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, padding=2),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, padding=2),
+            nn.Conv2d(1, 32, kernel_size=5, padding=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2))
-        self.fc = nn.Linear(2048, 10)
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=5, padding=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 32, kernel_size=5, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.fc = nn.Linear(1152, 10)
 
     def forward(self, x):
         in_size = x.size(0)
         out = self.layer1(x)
         out = self.layer2(out)
+        out = self.layer3(out)
         out = out.view(in_size, -1)
         out = self.fc(out)
         return out
     
-# --------------------------------------------------------------------------------------------
 def get_total_parms(module):
     
     total_parms = 0
     # Find the total number of parameters being trained
     for t in module.state_dict().values():
-        total_parms += np.prod(t.shape)
+        total_parms += int(np.prod(t.shape))
     return total_parms
         
 # Fixed manual seed
@@ -135,24 +141,26 @@ torch.manual_seed(267)
 # Choose the right argument for x
         
 # Instantiate a model
-net = ConvNet(input_size, hidden_size, num_classes).to(device=run_device)
+net = ConvNet(hidden_size, num_classes).to(device=run_device)
+#net = FlatNet(INPUT_SIZE, hidden_size, num_classes).to(device=run_device)
 print(net)
 total_net_parms = get_total_parms(net)
 print ("Total trainable parameters:", total_net_parms)
 
-# --------------------------------------------------------------------------------------------
-# Choose the right argument for x
 criterion = nn.CrossEntropyLoss() 
 optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate)
 
 # =============================================================================
 # Make a run
 # =============================================================================
+start_time = time.time()
+dtype = torch.float32
+
 for epoch in range(num_epochs):
     for i, data in enumerate(train_loader):
 
         images, labels = data
-        #images= images.view(-1, CHANNELS * 32 * 32)
+        
         # Put the images and labels in tensors on the run device
         images= Variable(images).to(device=run_device)
         labels= Variable(labels).to(device=run_device)
@@ -163,10 +171,13 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         
-        if (i + 1) % 100 == 0:
-            print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
-                  % (epoch + 1, num_epochs, i + 1, len(train_set) // batch_size, loss.data.item()))
+        if ((i + 1) % 100 == 0) or ((i+1) == (len(train_set) // batch_size)):
+            print("Epoch [{0:2d}/{1:2d}], Step [{2:3d}/{3:3d}], Loss: {4:4f}, Elapsed time: {5:4d} seconds" \
+                  .format(epoch + 1, num_epochs, i + 1, len(train_set) // batch_size, \
+                          loss.data.item(), int(time.time() - start_time)))
             
+end_time = time.time()       
+
 # =============================================================================
 # Display results summary
 # =============================================================================
@@ -201,7 +212,6 @@ for images, labels in test_loader:
         class_correct[label] += c[i].int()
         class_total[label] += 1
 
-# --------------------------------------------------------------------------------------------
 for i in range(num_classes):
     print('Accuracy of {0:3d} : {1:0.1f}%'.format(i, float(100 * class_correct[i]) / class_total[i]))
 #%%
@@ -234,6 +244,7 @@ with open(run_base + suffix + '_results.txt', 'w') as rfile:
     rfile.write("Batch Size: {0}\n".format(batch_size))
     rfile.write("Final loss: {0:0.4f}\n".format(loss.data.item()))
     rfile.write("Run device: {0}\n".format(run_device))
+    rfile.write("Elapsed time: {0:d}\n".format(int(end_time - start_time)))
     rfile.write('\n')
     rfile.write('Accuracy of the network on the {0:d} test images: {1:0.1f}%\n'.format(total, float(100 * correct) / total))
     rfile.write('\n')
