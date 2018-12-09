f085f6f132b13bfa588e4698451cdd6be94677ae
Darshan Kasat <darshankasat22@gwu.edu>
2018-12-08 19:07:59 -0500

    update the loop to normalize the image

--- code/caffe/preprocessing.py
+++ code/caffe/preprocessing.py
@@ -1,3 +1,10 @@
+"""
+Author: Darshan Kasat
+reference: https://blog.csdn.net/yj3254/article/details/52370767
+"""
+
+
+
 import caffe
 import numpy as np
 import lmdb
@@ -10,18 +17,18 @@ train_cursor = train_txn.cursor()
 datum = caffe_pb2.Datum()
 
 N=0
-mean = np.zeros((1, 3, 32, 32))
+mean_array = np.zeros((1, 3, 32, 32))
 for key, value in train_cursor:
     datum.ParseFromString(value)
     data = caffe.io.datum_to_array(datum)
     image = data.transpose(1,2,0)
-    mean[0, 0] += image[:, :, 0]
-    mean[0, 1] += image[:, :, 1]
-    mean[0, 2] += image[:, :, 2]
+    mean_array[0, 0] += image[:, :, 0]
+    mean_array[0, 1] += image[:, :, 1]
+    mean_array[0, 2] += image[:, :, 2]
     N = N+1
 
-mean[0] /= N
-blob = caffe.io.array_to_blobproto(mean)
+mean_array[0] /= N
+blob = caffe.io.array_to_blobproto(mean_array)
 with open('mean.binaryproto', 'wb') as f:
     f.write(blob.SerializeToString())
 
